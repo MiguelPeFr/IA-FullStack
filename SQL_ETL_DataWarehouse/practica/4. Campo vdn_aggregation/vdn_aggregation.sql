5e67bdd20f8cae0eaf7d29725898a48a3da30442
Miguel Pérez <miguelpef@outlook.com>
2024-09-21 12:00:04 +0200

    Cambios en la practica

--- SQL_ETL_DataWarehouse/practica/4. Campo vdn_aggregation/vdn_aggregation.sql
+++ SQL_ETL_DataWarehouse/practica/4. Campo vdn_aggregation/vdn_aggregation.sql
@@ -1,8 +1,12 @@
-SELECT   ivr_detail.calls_vdn_label,
+SELECT calls_ivr_id 
+    , MAX(vdn_aggregation)
+FROM 
+    (SELECT calls_ivr_id
+        ,CASE WHEN ivr_detail.calls_vdn_label LIKE "ATC%" THEN "FRONT"
+            WHEN ivr_detail.calls_vdn_label LIKE "TECH%" THEN "TECH"
+            WHEN ivr_detail.calls_vdn_label LIKE "ABSORPTION" THEN "ABSORPTION"
+            ELSE "RESTO"
+        END AS vdn_aggregation
+    FROM keepcoding.ivr_detail) AS subquery
 
-    CASE WHEN ivr_detail.calls_vdn_label LIKE "ATC%" THEN "FRONT"
-        WHEN ivr_detail.calls_vdn_label LIKE "TECH%" THEN "TECH"
-        WHEN ivr_detail.calls_vdn_label LIKE "ABSORPTION" THEN "ABSORPTION"
-        ELSE "RESTO"
-    END AS vdn_aggregation
-FROM keepcoding.ivr_detail;
+GROUP BY calls_ivr_id;
